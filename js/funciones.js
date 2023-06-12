e13c8f3779ce1da736eae680a1302ea507edcb3d
sarboleda72 <sarboleda72@misena.edu.co>
2023-06-12 16:46:33 -0500

    Se agregan 3 botones de ataque (Solo visual)

---
+++ js/funciones.js
@@ -0,0 +1,21 @@
+//Sección de cambio de imagen del boton
+function btnAguAba(elemento) {
+    elemento.src = "sprites/interfaz/botones/agua/abajo.png"
+  }
+function btnAguArr(elemento) {
+    elemento.src = "sprites/interfaz/botones/agua/arriba.png"
+  }
+
+function btnFueAba(elemento) {
+    elemento.src = "sprites/interfaz/botones/fuego/abajo.png"
+  }
+function btnFueArr(elemento) {
+    elemento.src = "sprites/interfaz/botones/fuego/arriba.png"
+  }
+
+function btnTruAba(elemento) {
+    elemento.src = "sprites/interfaz/botones/trueno/abajo.png"
+  }
+function btnTruArr(elemento) {
+    elemento.src = "sprites/interfaz/botones/trueno/arriba.png"
+  }
