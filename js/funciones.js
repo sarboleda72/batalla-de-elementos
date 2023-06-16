7bbcdcff15971064a3ba2a192133b37da95ded61
sarboleda72 <sarboleda72@misena.edu.co>
2023-06-15 21:11:21 -0500

    Se agregan efectos de sonido al juego al morir

--- js/funciones.js
+++ js/funciones.js
@@ -37,10 +37,12 @@ function ataAgu() {
     var dan= calDan(0,Math.floor(Math.random() * 3))
     var nueVid=(vida.offsetWidth-dan ).toString()+"px"; 
     menGol(dan);
-    vida.style.width=nueVid;
+    if(parseInt(nueVid)<1){vida.style.width="0px";}else{vida.style.width=nueVid;}
     setTimeout(function(){ataque.src="";
     defensa.src="sprites/Enemigo/espera.gif";
-    mensaje.src="sprites/interfaz/mensaje/defiendete.gif";
+    var fin=finJuego();
+    if(fin){return};  
+    mensaje.src="sprites/interfaz/mensaje/defiendete.gif";    
     asiDef();},3000 );
   }
 
@@ -57,10 +59,12 @@ function ataTru() {
     var dan= calDan(1,Math.floor(Math.random() * 3))
     var nueVid=(vida.offsetWidth-dan ).toString()+"px"; 
     menGol(dan);
-    vida.style.width=nueVid;
+    if(parseInt(nueVid)<1){vida.style.width="0px";}else{vida.style.width=nueVid;}
     setTimeout(function(){ataque.src="";
     defensa.src="sprites/Enemigo/espera.gif";
-    mensaje.src="sprites/interfaz/mensaje/defiendete.gif";
+    var fin=finJuego();
+    if(fin){return};   
+    mensaje.src="sprites/interfaz/mensaje/defiendete.gif";    
     asiDef();},3000 );
   }
   
@@ -102,12 +106,14 @@ function ataFue() {
             defensa.src="sprites/Enemigo/golpeado.gif";
             var dan= calDan(1,Math.floor(Math.random() * 3))
             var nueVid=(vida.offsetWidth-dan ).toString()+"px";         
-            vida.style.width=nueVid;
+            if(parseInt(nueVid)<1){vida.style.width="0px";}else{vida.style.width=nueVid;}
             menGol(dan);
             setTimeout(function(){
                 defensa.src="sprites/Enemigo/espera.gif";
                 reiniciarEstilos(imagen, hojaEstilos);
-                mensaje.src="sprites/interfaz/mensaje/defiendete.gif";
+                var fin=finJuego();
+                if(fin){return};   
+                mensaje.src="sprites/interfaz/mensaje/defiendete.gif";                
                 asiDef(); 
             },2000);                       
           }, 1000);
@@ -127,15 +133,15 @@ function reiniciarEstilos(imagen, hojaEstilos) {
 //Calcula el daño de los ataques
 function calDan(ata,def){   
   const combinaciones = {
-    "0-0": 20, // agua vs agua
-    "0-1": 40, // agua vs fuego
+    "0-0": 40, // agua vs agua
+    "0-1": 80, // agua vs fuego
     "0-2": 0, // agua vs rayo
     "1-0": 0, // fuego vs agua
-    "1-1": 20, // fuego vs fuego
-    "1-2": 40, // fuego vs rayo
+    "1-1": 40, // fuego vs fuego
+    "1-2": 80, // fuego vs rayo
     "2-0": 40, // rayo vs agua
     "2-1": 0, // rayo vs fuego
-    "2-2": 20 // rayo vs rayo
+    "2-2": 80 // rayo vs rayo
   };
   cal=ata.toString()+"-"+def.toString();
   return combinaciones[cal]
@@ -143,9 +149,9 @@ function calDan(ata,def){
 //envía mensaje dependiendo de la cantidad de ataque hecho
 function menGol(dan){
   var mensaje=document.querySelector(".mensaje");
-  if(dan==40){
+  if(dan==80){
     mensaje.src="sprites/interfaz/mensaje/golpecritico.gif";
-  }else if(dan==20){
+  }else if(dan==40){
     mensaje.src="sprites/interfaz/mensaje/golpeacertado.gif";
   }else if(dan==0){
     mensaje.src="sprites/interfaz/mensaje/fallocritico.gif" ;
@@ -207,9 +213,34 @@ function opoAta(dan,tipAta,valAta,tipBot) {
   dan= calDan(valAta,dan);
   var nueVid=(vida.offsetWidth-dan ).toString()+"px"; 
   menGol(dan);
-  vida.style.width=nueVid;
+  if(parseInt(nueVid)<1){vida.style.width="0px";}else{vida.style.width=nueVid;}  
   setTimeout(function(){ataque.src="";
   defensa.src="sprites/Atacante/espera.gif";
-  mensaje.src="sprites/interfaz/mensaje/ataca.gif";
-  asiAta()},3000 );
+  var fin=finJuego();
+  if(fin){return};   
+  mensaje.src="sprites/interfaz/mensaje/ataca.gif";  
+  asiAta();},3000 );
+}
+
+function finJuego(){
+  var atacante=document.querySelector(".vidAta");
+  var imgAta=document.querySelector('.pnjAta');
+  var imgEne=document.querySelector('.pnjEne');
+  var enemigo=document.querySelector(".vidEne");
+  var snd=document.querySelector(".sonido");
+  var mensaje=document.querySelector(".mensaje");
+  if(atacante.offsetWidth<1){
+    bloBot();
+    imgAta.src="sprites/Atacante/muere.gif";
+    snd.src="sonidos/muerte.mp3";
+    snd.play();
+    return true 
+  }
+  if(enemigo.offsetWidth<1){
+    bloBot();
+    imgEne.src="sprites/Enemigo/muere.gif";    
+    snd.src="sonidos/ganador.mp3";
+    snd.play();
+    return true
+  }
 }
